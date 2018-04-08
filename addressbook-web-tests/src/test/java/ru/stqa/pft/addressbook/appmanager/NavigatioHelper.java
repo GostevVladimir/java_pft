24c62f296d09ad57d7ee6a678442975da010ad69
GostevVladimir <vladi-gos@yandex.ru>
2018-04-08 13:11:22 +0400

    Создал общий базовый класс для классов помщников.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
@@ -3,14 +3,13 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigatioHelper {
-  private FirefoxDriver wd;
+public class NavigatioHelper extends HelperBase{
 
   public NavigatioHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-      wd.findElement(By.linkText("groups")).click();
+      click(By.linkText("groups"));
   }
 }
