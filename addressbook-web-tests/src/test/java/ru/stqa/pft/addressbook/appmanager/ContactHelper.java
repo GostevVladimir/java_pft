7bae6db3c083edb41ed33bdc7631c18a004fecb7
GostevVladimir <vladi-gos@yandex.ru>
2018-04-08 15:51:34 +0400

    Выполнил Задание №6: Сделать тесты для удаления и модификации групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -43,4 +43,12 @@ public class ContactHelper extends HelperBase{
   public void acceptAlert() {
     wd.switchTo().alert().accept();
   }
+
+  public void initContactModification() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModificatio() {
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTest extends TestBase{
+
+  @Test
+  public void testContactModification(){
+    app.getNavigatioHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("FirstNameTest1-1", "LastNameTest2-2", "Penza",
+            "252525", "test1@mail.com"));
+    app.getContactHelper().submitContactModificatio();
+    app.getContactHelper().returnToHomePage();
+  }
+}
