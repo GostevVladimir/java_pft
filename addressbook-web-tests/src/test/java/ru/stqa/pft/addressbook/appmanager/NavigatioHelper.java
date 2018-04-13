09c1ac67f9c0ab73eeb7c9747d381c4616302348
VladimirGostev <vladi-gos@yandex.ru>
2018-04-13 11:36:22 +0300

    Реализовал конструкцию для запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigatioHelper.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigatioHelper extends HelperBase{
 
-  public NavigatioHelper(FirefoxDriver wd) {
+  public NavigatioHelper(WebDriver wd) {
     super(wd);
   }
 
